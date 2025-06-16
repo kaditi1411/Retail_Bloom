e309d4e9415dfb1cd890281b46ad61fe4809ad92
Nayak1079 <sujalnayak41@gmail.com>
2025-06-16 16:27:31 +0530

    Removed  folder

--- model/app/utils.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-def load_sales_data(path):
-    df = pd.read_csv(path, parse_dates=["Date"])
-    df = df.groupby(["Product Category", "Date"], as_index=False).agg({"Total Amount": "sum"})
-    df = df.rename(columns={"Product Category": "SKU_ID", "Total Amount": "Sales_Quantity"})
-    return df.sort_values(["SKU_ID", "Date"])
-
